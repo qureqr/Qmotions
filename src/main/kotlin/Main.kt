6b82d5418cdecd094af1328fbf6e4bf6ee2554ff
kaufmoo <pozzibros12345@gmail.com>
2025-09-02 02:39:36 +0200

    подключение зависимостей

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.qure
 
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import org.qure.ui.EmotionVisualizerScreen
+
 fun main() {
     println("Hello World!")
 }
--- src/main/kotlin/ui/EmotionVisualizerScreen.kt
+++ src/main/kotlin/ui/EmotionVisualizerScreen.kt
@@ -1,4 +1,16 @@
 package org.qure.ui
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.qure.data.Emotions
+
 class EmotionVisualizerScreen {
 }
